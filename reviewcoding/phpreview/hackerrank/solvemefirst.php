d9b1ab48a1b28ae7cd164ddebf24be3bdb0a27de
Aaron <phantommz@qq.com>
2016-03-05 16:32:15 +0800

    mysql join

--- reviewcoding/phpreview/hackerrank/solvemefirst.php
+++ reviewcoding/phpreview/hackerrank/solvemefirst.php
@@ -1,10 +1,13 @@
 <?php
+
 	function solvemefirst($a,$b){
 		return $a + $b;
 	}
 	$read=fopen("php://stdin","in");
 	$_a=fgets($read);
 	$_b=fgets($read);
-	echo $sum=solvemefirst(int($_a),int($_b);
+	$sum=solvemefirst(($_a),($_b));
 	fclose($read);
+	echo $sum;
+
 ?>
