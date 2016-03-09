aa1167fbc3322da1abe19c6e30cdb19e8af87f09
Aaron <phantommz@qq.com>
2016-03-09 12:14:46 +0800

    push again

---
+++ git/quickstart/app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    //
+	public function index(){
+
+		return "life is like a strug";
+	
+	}
+}
